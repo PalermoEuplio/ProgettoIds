dc912331474f395aeaac94231101e6df71adc132
PalermoEuplio <euppal123456789@gmail.com>
2025-12-04 16:22:08 +0100

    j

---
+++ src/main/java/gruppo20/biblioteca/GestionePrestiti.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gruppo20.biblioteca;
+import java.util.*;
+/**
+ *
+ * @author Osv
+ */
+public class GestionePrestiti {
+    private TreeSet<Prestito> Prestiti;
+    private final String nome;
+   
+    public GestionePrestiti(String nome){
+        this.nome = nome;
+        this.Prestiti = new TreeSet<>();
+    }
+    //restituisce direttamente una stringa con la libreria
+    public String getPrestiti() {
+        return Prestiti.toString();//fare override toString
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void addPrestito (Prestito p){
+        Prestiti.add(p);
+    }
+    
+    public void delPrestito (Prestito p){
+        Prestiti.remove(p);
+    }
+
+    public boolean hasPrestito (Prestito p){
+        return Prestiti.contains(p);
+    }
+      
+}
---
+++ src/main/java/gruppo20/biblioteca/Libro.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppo20.biblioteca;
+
+import java.time.*;
+import java.util.*;
+
+/**
+ *
+ * @author giuli
+ */
+public class Libro {
+    
+    
+    private String titolo;
+    private String[] autori;
+    private LocalDate date;
+    private int nCopie;
+    private int isbn;
+
+    public Libro(String titolo, String[] autori, LocalDate date, int nCopie, int isbn) {
+        this.titolo = titolo;
+        this.autori=autori;
+        this.date = date;
+        this.nCopie = nCopie;
+        this.isbn = isbn;
+    }
+    
+    
+    
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setAutori(String[] autori) {
+        this.autori = autori;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setnCopie(int nCopie) {
+        this.nCopie = nCopie;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+    
+    
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String[] getAutori() {
+        return autori;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getnCopie() {
+        return nCopie;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Titolo=" + titolo + ", autori=" + autori + ", date=" + date + ", nCopie=" + nCopie + ", isbn=" + isbn;
+    } 
+    
+}
