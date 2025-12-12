ef1e6e2a410144c6395ec160b70989d49aee6b9f
'RiccardolpX2' <r.laporta6@studenti.unisa.it>
2025-12-12 16:06:20 +0100

    Implementazione JUnit 6.0.1

--- src/test/java/gruppo20/biblioteca/model/Prestiti/PrestitoTest.java
+++ src/test/java/gruppo20/biblioteca/model/Prestiti/PrestitoTest.java
@@ -5,12 +5,10 @@
 package gruppo20.biblioteca.model.Prestiti;
 
 import java.time.LocalDate;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
 
 /**
  *
