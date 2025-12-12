8ae48c48e1c84802e3ecc2caef2099748c8bcf62
'RiccardolpX2' <r.laporta6@studenti.unisa.it>
2025-12-12 16:49:08 +0100

    Implementazione testGetDataResituzione e testGetDataPrestito, test parametrizzati

--- src/test/java/gruppo20/biblioteca/model/Prestiti/PrestitoTest.java
+++ src/test/java/gruppo20/biblioteca/model/Prestiti/PrestitoTest.java
@@ -5,9 +5,13 @@
 package gruppo20.biblioteca.model.Prestiti;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 /**
@@ -19,53 +23,59 @@ public class PrestitoTest {
     public PrestitoTest() {
         System.out.println("inizio prestito test");
         Prestito notRest = new Prestito(LocalDate.MIN, LocalDate.MIN, "false", "Analisi1", "0000000000000","0612700000");
-    }
-    
+    }   
     
     
     @Test
     public void testSetEffettivaRestituzione() {
         LocalDate input = LocalDate.of(2025,12,12);
         String ritorno = input.toString();
-        System.out.println("setEffettivaRestituzione");
-        
+        //System.out.println("setEffettivaRestituzione");        
         LocalDate dataRestituzione = LocalDate.now();
+        
         Prestito instance = new Prestito(LocalDate.MIN, LocalDate.MIN, "false", "Analisi1", "0000000000000","0612700000");       
         instance.setEffettivaRestituzione(input);
         assertEquals(ritorno, instance.getEffettivaRestituzione());
         //fail("The test case is a prototype.");
     }
-    @Test
+    @Test    
     public void testSetEffettivaRestituzione2(){
-        LocalDate input = LocalDate.of(2025,12,12);
-        String ritorno = input.toString();
-        System.out.println("setEffettivaRestituzione2");
         
-        LocalDate dataRestituzione = LocalDate.now();
-        Prestito instance = new Prestito(LocalDate.MIN, LocalDate.MIN, ritorno, "Analisi1", "0000000000000","0612700000");       
-        instance.setEffettivaRestituzione(input);
+        LocalDate input = LocalDate.of(2025,12,12);
+        LocalDate dataRestituzione = input.plusDays(30);
+        String ritorno = dataRestituzione.toString();        
+        System.out.println("setEffettivaRestituzione2");
+        Prestito instance = new Prestito(input, null, ritorno, "Analisi_1", "0000000000000","0612700000");       
+
+        
+       
+        instance.setEffettivaRestituzione(dataRestituzione);
         assertEquals(ritorno, instance.getEffettivaRestituzione());
     }
     
 
-    @Test
-    public void testGetDataPrevistaRestituzione() {
-        System.out.println("getDataPrevistaRestituzione");
-        Prestito instance = null;
-        LocalDate expResult = null;
+    
+    @ParameterizedTest
+    @MethodSource("dateGen")    
+    public void testGetDataPrevistaRestituzione(LocalDate in) {
+        System.out.println("getDataPrevistaRestituzione test");
+        Prestito instance = new Prestito(in, in.plusDays(30), "false", "Analisi1", "0000000000000","0612700000");
+        LocalDate expResult = in.plusDays(30);
         LocalDate result = instance.getDataPrevistaRestituzione();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
     }
 
-    @Test
-    public void testGetDataPrestito() {
+    @ParameterizedTest
+    @MethodSource("dateGen")
+    public void testGetDataPrestito(LocalDate in) {
         System.out.println("getDataPrestito");
-        Prestito instance = null;
-        LocalDate expResult = null;
+        Prestito instance = new Prestito(in, in.plusDays(30), "false", "Analisi1", "0000000000000","0612700000");
+
+        LocalDate expResult = in;
         LocalDate result = instance.getDataPrestito();
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
     }
 
     @Test
@@ -147,6 +157,11 @@ public class PrestitoTest {
         int result = instance.hashCode();
         assertEquals(expResult, result);
         fail("The test case is a prototype.");
+    }
+    static Stream<LocalDate> dateGen() {
+        LocalDate oggi = LocalDate.now();        
+        return Stream.iterate(oggi, data -> data.plusDays(1)).limit(50);
+                     
     }
     
 }
